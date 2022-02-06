dda90dd5fcc0f6bc4dd3ffa4afd227b90e120a46
John Cline <2057878+clinejj@users.noreply.github.com>
2022-02-06 17:47:28 -0500

    Remove module include

--- lib/paper_trail_association_tracking/frameworks/active_record/models/paper_trail/version.rb
+++ lib/paper_trail_association_tracking/frameworks/active_record/models/paper_trail/version.rb
@@ -3,10 +3,6 @@
 require "paper_trail_association_tracking/version_concern"
 
 module PaperTrail
-  module VersionConcern
-    include ::PaperTrailAssociationTracking::VersionConcern
-  end
-
   class Version < ::ActiveRecord::Base
     include ::PaperTrailAssociationTracking::VersionConcern
   end
